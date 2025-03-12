8f3c0fbadd6fedf841da630f66a462f6211d8223
Ruben Hernandez <hernandzruben9@gmail.com>
2025-03-12 19:26:20 -0400

    sss

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ from repository.messagesRepository import MessageRepository
 
 app = FastAPI()
 
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_origin_regex=".*",  # Permite todos los orígenes mediante una expresión regular
-    allow_credentials=True,   # Permite el envío de credenciales
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["https://chatbot-pcc-cujae.vercel.app"],  # Solo permite este origen
+    allow_credentials=True,  # Permite el envío de credenciales
+    allow_methods=["*"],  # Permite todos los métodos HTTP
+    allow_headers=["*"],  # Permite todos los encabezados
 )
 
 app.include_router(endpoints.router)
